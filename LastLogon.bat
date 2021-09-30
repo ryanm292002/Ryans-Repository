f22e18995d0a63d822d0578cbdbc0fc1cbc67991
ryanm292002 <71150667+ryanm292002@users.noreply.github.com>
2021-09-30 12:17:25 -0400

    Add files via upload

---
+++ LastLogon.bat
@@ -0,0 +1,4 @@
+cmd /c start %windir%\System32\WindowsPowershell\v1.0\powershell.exe -windowstyle hidden -ExecutionPolicy Bypass -file "%~dp0lastLogon.ps1"
+
+
+PAUSE
---
+++ lastLogon.ps1
@@ -0,0 +1,8 @@
+﻿#Get the userprofile environment variable 
+$user = $ENV:USERPROFILE
+
+#Get the current date and time
+$theTime = Get-Date
+
+#Write a message with the time to a file as a string.
+echo "You logged in at: $theTime" | Out-File -Encoding ASCII -Append -FilePath "$user\Desktop\Login-time.txt"
