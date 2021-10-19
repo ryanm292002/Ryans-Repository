7e8503094bc17bf05a6e8970c9b9e213311ce642
ryanm292002 <71150667+ryanm292002@users.noreply.github.com>
2021-10-19 11:35:14 -0400

    Add files via upload

---
+++ Classwork/Searchlogs_noSearchPrompt.ps1
@@ -0,0 +1,15 @@
+﻿#Storyline: Reviewing the Security Event Log
+
+# Directory to save files
+$myDir ="C:\Users\ryan.morrissey-adm\Desktop"
+
+#List all the available windows event logs
+Get-EventLog -list
+
+# Create a prompt to allow the user to select the log to view
+$readLog = Read-Host -Prompt "Please select a log to review from the list above"
+
+
+# Print the results for the log 
+Get-Eventlog -LogName $readLog -Newest 40 | where {$_.Message -ilike "*special privileges*"} |  export-csv -NoTypeInformation `
+-Path "$myDir\securitylog.csv"
