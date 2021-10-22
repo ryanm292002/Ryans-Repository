84dae1d7f331acbab5f88b03fecbd08b91581940
ryanm292002 <71150667+ryanm292002@users.noreply.github.com>
2021-10-22 13:42:58 -0400

    Update WMI-netinfo-RAM.ps1

--- WMI-netinfo-RAM.ps1
+++ WMI-netinfo-RAM.ps1
@@ -1,4 +1,15 @@
-﻿# Get-WmiObject -List | where { $_.Name -ilike "Win32_[n]*" } | sort-object (Code to find out which Class to use, looking for network info so sorted by n)
- #Get-WmiObject -Class Win32_NetworkAdapter | Get-Member -MemberType Property (This wasnt the right class, at least I couldnt find anything useful but I kept it here anyways)
-#Get-WmiObject -Class Win32_NetworkAdapterConfiguration | Get-Member -MemberType Property (Gave class list for network adapter config, looked for required classes)
-Get-WmiObject -Class Win32_NetworkAdapterConfiguration | select IPAddress, DefaultIPGateway, DHCPServer, DNSServerSearchorder
+# STORY LINE:
+# These are some examples of how Get-WmiObject can be useful for you powershell system, you can use it to find information such as
+# properties of your network adapter
+
+#Code to find out which Class to use, looking for network info so sorted by n
+# Get-WmiObject -List | where { $_.Name -ilike "Win32_[n]*" } | sort-object 
+
+(This wasnt the right class, at least I couldnt find anything useful but I kept it here anyways)
+ #Get-WmiObject -Class Win32_NetworkAdapter | Get-Member -MemberType Property 
+ 
+ #(Gave class list for network adapter config, looked for required classes)
+#Get-WmiObject -Class Win32_NetworkAdapterConfiguration | Get-Member -MemberType Property 
+
+#choose specifically what properties you want to look at of the adapter configuration.
+Get-WmiObject -Class Win32_NetworkAdapterConfiguration | select IPAddress, DefaultIPGateway, DHCPServer, DNSServerSearchorder 
