1ff413a24f0bf2f3b3ad535eb6b800ef721263bd
ryanm292002 <71150667+ryanm292002@users.noreply.github.com>
2021-10-26 12:22:39 -0400

    Add files via upload

---
+++ Classwork/SSH script.ps1
@@ -0,0 +1,26 @@
+﻿# SSH to a remote host and copy files to our remote host
+#Find-Module *ssh*
+#Install-Module Posh-SSH -Scope CurrentUser
+#Get-Command -Module Posh-SSH
+
+#create a new SSH session
+New-SSHSession -ComputerName 'web01-ryan' -Credential (get-credential ryan)
+
+
+#run infintley, instead of asking for password before each command
+while ($true) {
+
+#create a prompt for the command
+$the_cmd = Read-Host -Prompt "Please enter a command"
+
+# Look for the keyword exit and stop executing or itll keep running invoke-sshcommand
+if ($the_cmd -eq "exit") {
+
+break
+
+}
+
+#Run a command on the remote system
+(Invoke-SSHCommand -index 0 $the_cmd).output
+
+}
---
+++ Classwork/sendfiletoremotesystem.ps1
@@ -0,0 +1,5 @@
+﻿# send a file to  a remote system
+
+#use the set-SCPItem to send a file to  a remote system
+#you would use get-SCPItem file to download a file from a remote system
+Set-SCPItem -ComputerName web01-ryan (Get-Credential ryan) -Destination '/home/ryan' -Path '.\tosend.txt'
