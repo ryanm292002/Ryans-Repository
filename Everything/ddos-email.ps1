8308278b48a185dce5f0ae5e2ea795f0abfc5390
ryanm292002 <71150667+ryanm292002@users.noreply.github.com>
2021-11-11 12:39:01 -0500

    Add files via upload

---
+++ Everything/ddos-email.ps1
@@ -0,0 +1,12 @@
+﻿#DO not do this in real life
+
+$email = " ryan@ryan.local"
+
+while ($true) {
+
+
+}
+
+DDOS SMTP server
+
+1. create an A record with a low priority and an IP that dosnt exist so basically the ddos attacks nothing
