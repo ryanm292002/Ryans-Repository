01aa941d1a929af09e3e1cce07f02c25b7b7bd3d
ryanm292002 <ryan.morrissey@mymail.champlain.edu>
2022-02-20 07:52:52 +0000

    done

--- SYS265/linux/centos7/secure-ssh.sh
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -3,4 +3,9 @@
 #$@creates a new ssh user user $parameter
 #adds a public key from the local repo or curled from the remote repo
 #removes roots ability to ssh in
-echo "ALL YOUR CODE GOES HERE"
+sudo useradd -m -d /home/$1 -s /bin/bash $1;
+sudo mkdir /home/$1/.ssh;
+sudo cp SYS265/linux/public-keys/id_rsa.pub /home/$1/.ssh/authorized_keys;
+sudo chmod 700 /home/$1/.ssh;
+sudo chmod 600 /home/$1/.ssh/authorized_keys;
+sudo chown -R $1:$1 /home/$1/.ssh;
