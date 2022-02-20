f5daa474c8ce1935769920258b0f0faafb97f6a0
ryanm292002 <ryan.morrissey@mymail.champlain.edu>
2022-02-20 01:01:25 -0500

    commit

---
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -0,0 +1,6 @@
+#secure-ssh.sh
+#author ryanm292002
+@creates a new ssh user user $parameter
+#adds a public key from the local repo or curled from the remote repo
+#removes roots ability to ssh in
+echo "ALL YOUR CODE GOES HERE"
